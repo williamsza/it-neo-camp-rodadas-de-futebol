e6696aaefe4c15253cbc87b12c7dd8e0a44f3efa
“williamsza” <wyllsza@gmail.com>
2025-06-27 17:42:06 -0300

    criada estrutura de pastas

---
+++ rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/RodadasDeFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.it_neo_camp.rodadas_de_futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class RodadasDeFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodadasDeFutebolApplication.class, args);
+	}
+
+}
---
+++ rodadas-de-futebol/src/test/java/br/com/it_neo_camp/rodadas_de_futebol/RodadasDeFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.it_neo_camp.rodadas_de_futebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodadasDeFutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
