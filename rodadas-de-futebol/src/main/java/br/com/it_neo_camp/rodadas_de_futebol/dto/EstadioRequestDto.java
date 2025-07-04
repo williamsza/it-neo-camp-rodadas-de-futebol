adfe71f26ae0ce5eec66ed1af21193cafcb436bf
“williamsza” <wyllsza@gmail.com>
2025-07-04 15:51:39 -0300

    Criacao da classe EstadioRequestDto

--- rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/dto/EstadioRequestDto.java
+++ rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/dto/EstadioRequestDto.java
@@ -3,7 +3,7 @@ package br.com.it_neo_camp.rodadas_de_futebol.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class EstadioDto {
+public class EstadioRequestDto {
 
     @NotBlank(message = "Nome do estádio é obrigatório.")
     @Size(min = 3, message = "Nome do estádio deve ter ao menos 3 caracteres.")
