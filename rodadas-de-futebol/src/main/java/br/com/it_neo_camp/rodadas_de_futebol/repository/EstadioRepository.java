c451d6cdd7b54e6d9f18e3ea402e25bb6745e94b
“williamsza” <wyllsza@gmail.com>
2025-07-04 16:20:07 -0300

    Ajuste da interface EstadioRepository

--- rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/repository/EstadioRepository.java
+++ rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/repository/EstadioRepository.java
@@ -1,2 +1,7 @@
-package br.com.it_neo_camp.rodadas_de_futebol.repository;public class EstadioRepository {
+package br.com.it_neo_camp.rodadas_de_futebol.repository;
+
+import br.com.it_neo_camp.rodadas_de_futebol.model.Estadio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadioRepository extends JpaRepository<Estadio, Long> {
 }
