18157ad3d7a284d8c42c313ff99a1508e84fab56
“williamsza” <wyllsza@gmail.com>
2025-07-04 15:42:31 -0300

    Ajuste da classe Estadio

--- rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/model/Estadio.java
+++ rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/model/Estadio.java
@@ -1,2 +1,23 @@
-package br.com.it_neo_camp.rodadas_de_futebol.model;public class Estadio {
+package br.com.it_neo_camp.rodadas_de_futebol.model;
+
+public class Estadio {
+
+    private Long estadioId;
+    private String nomeEstadio;
+
+    public Long getEstadioId() {
+        return estadioId;
+    }
+
+    public void setEstadioId(Long estadioId) {
+        this.estadioId = estadioId;
+    }
+
+    public String getNomeEstadio() {
+        return nomeEstadio;
+    }
+
+    public void setNomeEstadio(String nomeEstadio) {
+        this.nomeEstadio = nomeEstadio;
+    }
 }
