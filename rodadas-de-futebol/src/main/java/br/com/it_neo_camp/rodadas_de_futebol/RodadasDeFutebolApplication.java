d45dd6a3b0ce74dda0e31027d94f2f79243747b7
“williamsza” <wyllsza@gmail.com>
2025-07-07 17:52:59 -0300

    Ajuste do application.proprieties, do pom

--- rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/RodadasDeFutebolApplication.java
+++ rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/RodadasDeFutebolApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan("br.com.it_neo_camp.rodadas_de_futebol.model") // Adicione esta linha
 @SpringBootApplication
 class RodadasDeFutebolApplication {
 
