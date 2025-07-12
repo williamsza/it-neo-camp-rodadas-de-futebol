2f89745663e163d77623dac03bd9ad272aec0c49
williamsza <wyllsza@gmail.com>
2025-07-12 12:04:39 -0300

    Atualização da entidade Estadio

---
+++ rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/controller/PartidaController.java
@@ -0,0 +1,4 @@
+package br.com.it_neo_camp.rodadas_de_futebol.controller;
+
+public class PartidaController {
+}
--- rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/model/Estadio.java
+++ rodadas-de-futebol/src/main/java/br/com/it_neo_camp/rodadas_de_futebol/model/Estadio.java
@@ -1,6 +1,8 @@
 package br.com.it_neo_camp.rodadas_de_futebol.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "estadios")
@@ -9,6 +11,9 @@ public class Estadio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long estadioId;
+    @NotBlank(message = "O nome do estádio é obrigatório.")
+    @Size(min = 3, message = "O nome do estádio deve ter no mínimo 3 caracteres.")
+    @Column(unique = true, nullable = false)
     private String nomeEstadio;
 
     public Long getEstadioId() {
