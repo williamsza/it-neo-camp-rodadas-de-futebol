fa554c418b5bbfd1ba97c66058593f5106321c52
“williamsza” <wyllsza@gmail.com>
2025-07-24 17:53:47 -0300

    Refatoracao dos testes do metodo cadastrarPartida.

--- rodadas-de-futebol/src/test/java/br/com/it_neo_camp/rodadas_de_futebol/service/PartidaServiceTest.java
+++ rodadas-de-futebol/src/test/java/br/com/it_neo_camp/rodadas_de_futebol/service/PartidaServiceTest.java
@@ -3,6 +3,7 @@ package br.com.it_neo_camp.rodadas_de_futebol.service;
 import br.com.it_neo_camp.rodadas_de_futebol.dto.request.PartidaRequestDto;
 import br.com.it_neo_camp.rodadas_de_futebol.exception.ConflitoDadosException;
 import br.com.it_neo_camp.rodadas_de_futebol.exception.DadosInvalidosException;
+import br.com.it_neo_camp.rodadas_de_futebol.exception.PlacarInvalidoException;
 import br.com.it_neo_camp.rodadas_de_futebol.exception.RecursoNaoEncontradoException;
 import br.com.it_neo_camp.rodadas_de_futebol.model.Clube;
 import br.com.it_neo_camp.rodadas_de_futebol.model.Estadio;
@@ -17,192 +18,180 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class PartidaServiceTest {
 
+    private PartidaRepository partidaRepository;
+    private ClubeRepository clubeRepository;
+    private EstadioRepository estadioRepository;
+    private PartidaService partidaService;
+
+    private Long clubeMandanteId = 2L;
+    private Long clubeVisitanteId = 3L;
+    private Long estadioId = 1L;
     private LocalDateTime dataHora;
+
 
     @BeforeEach
     void setUp() {
+
+        partidaRepository = Mockito.mock(PartidaRepository.class);
+        clubeRepository = Mockito.mock(ClubeRepository.class);
+        clubeRepository = Mockito.mock(ClubeRepository.class);
+        estadioRepository = Mockito.mock(EstadioRepository.class);
+        partidaService = new PartidaService(partidaRepository, clubeRepository, estadioRepository);
+
+        dataHora = LocalDateTime.now().plusDays(1);
+
+        when(clubeRepository.findById(clubeVisitanteId))
+                .thenReturn(Optional.of(new Clube(clubeVisitanteId, "Visitante", true, dataHora.minusYears(1))));
+        when(estadioRepository.findById(estadioId))
+                .thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
+        when(partidaRepository.existsByEstadioIdAndDataHora(any(Long.class), any(LocalDateTime.class)))
+                .thenReturn(false);
+        when(partidaRepository.existsByClubeMandanteIdAndClubeVisitanteIdAndDataHora(
+                any(Long.class), any(Long.class), any(LocalDateTime.class)))
+                .thenReturn(false);
+        when(clubeRepository.findById(clubeMandanteId))
+                .thenReturn(Optional.of(new Clube(clubeMandanteId, "Mandante", true, dataHora.minusYears(1))));
+
+        // verify(partidaRepository, times(1)).save(any());
+
+
     }
+
 
     @Test
     void deveLancarExcecaoQuandoClubesMandanteEVisitanteSaoIguais() {
-        PartidaService service = new PartidaService(null, null, null);
-        PartidaRequestDto request = new PartidaRequestDto();
-        request.setClubeMandanteId(1L);
-        request.setClubeVisitanteId(1L);
-        request.setEstadioId(1L);
-        request.setPlacarMandante(0);
-        request.setPlacarVisitante(0);
-        request.setDataHora(LocalDateTime.now().plusDays(1));
-        assertThrows(DadosInvalidosException.class, () -> service.cadastrarPartida(request));
 
+        Mockito.when(clubeRepository.findById(1L))
+                .thenReturn(Optional.of(new Clube(1L, "Clube Teste", true, LocalDateTime.now().minusYears(1))));
+
+        PartidaRequestDto request = new PartidaRequestDto(1L, 1L, estadioId, 0, 0, dataHora);
+        assertThrows(DadosInvalidosException.class, () -> partidaService.cadastrarPartida(request));
 
     }
 
     @Test
     void deveLancarExcecaoQuandoEstadioOcupadoNoMesmoHorario() {
-        PartidaRepository repository = Mockito.mock(PartidaRepository.class);
-        ClubeRepository clubeRepository = Mockito.mock(ClubeRepository.class);
-        EstadioRepository estadioRepository = Mockito.mock(EstadioRepository.class);
 
-        Long clubeMandanteId = 2L;
-        Long clubeVisitanteId = 3L;
-        Long estadioId = 1L;
-        dataHora = LocalDateTime.now().plusDays(1);
+        when(partidaRepository.existsByEstadioIdAndDataHora(estadioId, dataHora)).thenReturn(true);
 
-        Mockito.when(repository.existsByEstadioIdAndDataHora(estadioId, dataHora)).thenReturn(true);
-        Mockito.when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "Mandante", true, dataHora.minusYears(1))));
-        Mockito.when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "Visitante", true, dataHora.minusYears(1))));
-        Mockito.when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
+        PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
 
-        PartidaService service = new PartidaService(repository, clubeRepository, estadioRepository);
-        PartidaRequestDto request = new PartidaRequestDto();
-
-        request.setClubeMandanteId(clubeMandanteId);
-        request.setClubeVisitanteId(clubeVisitanteId);
-        request.setEstadioId(estadioId);
-        request.setPlacarMandante(0);
-        request.setPlacarVisitante(0);
-        //request.setDataHora(LocalDateTime.now().plusDays(1));
-        request.setDataHora(dataHora);
-        assertThrows(ConflitoDadosException.class, () -> service.cadastrarPartida(request));
+        assertThrows(ConflitoDadosException.class, () -> partidaService.cadastrarPartida(request));
     }
 
     @Test
     void deveLancarExcecaoQuandoClubeMandanteNaoExiste() {
-        PartidaRepository repository = Mockito.mock(PartidaRepository.class);
-        ClubeRepository clubeRepository = Mockito.mock(ClubeRepository.class);
-        EstadioRepository estadioRepository = Mockito.mock(EstadioRepository.class);
 
-        Long clubeMandanteId = 2L;
-        Long clubeVisitanteId = 3L;
-        Long estadioId = 1L;
-        LocalDateTime dataHora = LocalDateTime.now().plusDays(1);
+        when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.empty());
 
-        Mockito.when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.empty());
-        Mockito.when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "visitante", true, dataHora.minusYears(1))));
-        Mockito.when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
-
-        PartidaService service = new PartidaService(repository, clubeRepository, estadioRepository);
         PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
 
-        assertThrows(RecursoNaoEncontradoException.class, () -> service.cadastrarPartida(request));
+        assertThrows(RecursoNaoEncontradoException.class, () -> partidaService.cadastrarPartida(request));
 
 
     }
 
     @Test
     void deveLancarExcecaoQuandoClubeVisitanteNaoExiste() {
-        PartidaRepository repository = Mockito.mock(PartidaRepository.class);
-        ClubeRepository clubeRepository = Mockito.mock(ClubeRepository.class);
-        EstadioRepository estadioRepository = Mockito.mock(EstadioRepository.class);
 
-        Long clubeMandanteId = 2L;
-        Long clubeVisitanteId = 3L;
-        Long estadioId = 1L;
-        LocalDateTime dataHora = LocalDateTime.now().plusDays(1);
+        when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", true, dataHora.minusYears(1))));
+        when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.empty());
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
 
-        Mockito.when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", true, dataHora.minusYears(1))));
-        Mockito.when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.empty());
-        Mockito.when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
-
-        PartidaService service = new PartidaService(repository, clubeRepository, estadioRepository);
         PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
 
-        assertThrows(RecursoNaoEncontradoException.class, () -> service.cadastrarPartida(request));
+        assertThrows(RecursoNaoEncontradoException.class, () -> partidaService.cadastrarPartida(request));
     }
 
     @Test
     void deveLancarExcecaoQuandoEstadioNaoExiste() {
 
-        PartidaRepository repository = Mockito.mock(PartidaRepository.class);
-        ClubeRepository clubeRepository = Mockito.mock(ClubeRepository.class);
-        EstadioRepository estadioRepository = Mockito.mock(EstadioRepository.class);
+        when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", true, dataHora.minusYears(1))));
+        when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "visitante", true, dataHora.minusYears(1))));
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.empty());
 
-        Long clubeMandanteId = 2L;
-        Long clubeVisitanteId = 3L;
-        Long estadioId = 1L;
-        LocalDateTime dataHora = LocalDateTime.now().plusDays(1);
-
-        Mockito.when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", true, dataHora.minusYears(1))));
-        Mockito.when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "visitante", true, dataHora.minusYears(1))));
-        Mockito.when(estadioRepository.findById(estadioId)).thenReturn(Optional.empty());
-
-        PartidaService service = new PartidaService(repository, clubeRepository, estadioRepository);
         PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
 
-        assertThrows(RecursoNaoEncontradoException.class, () -> service.cadastrarPartida(request));
-
+        assertThrows(RecursoNaoEncontradoException.class, () -> partidaService.cadastrarPartida(request));
 
     }
 
     @Test
     void deveLancarExcecaoQuandoClubeMandanteEstaInativo() {
-        PartidaRepository repository = Mockito.mock(PartidaRepository.class);
-        ClubeRepository clubeRepository = Mockito.mock(ClubeRepository.class);
-        EstadioRepository estadioRepository = Mockito.mock(EstadioRepository.class);
 
-        Long clubeMandanteId = 2L;
-        Long clubeVisitanteId = 3L;
-        Long estadioId = 1L;
-        LocalDateTime dataHora = LocalDateTime.now().plusDays(1);
+        when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", false, dataHora.minusYears(1))));
+        when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "visitante", true, dataHora.minusYears(1))));
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
 
-        Mockito.when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", false, dataHora.minusYears(1))));
-        Mockito.when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "visitante", true, dataHora.minusYears(1))));
-        Mockito.when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
-
-        PartidaService service = new PartidaService(repository, clubeRepository, estadioRepository);
         PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
 
-        assertThrows(ConflitoDadosException.class, () -> service.cadastrarPartida(request));
+        assertThrows(ConflitoDadosException.class, () -> partidaService.cadastrarPartida(request));
     }
 
     @Test
     void deveLancarExcecaoQuandoClubeVisitanteEstaInativo() {
-        PartidaRepository repository = Mockito.mock(PartidaRepository.class);
-        ClubeRepository clubeRepository = Mockito.mock(ClubeRepository.class);
-        EstadioRepository estadioRepository = Mockito.mock(EstadioRepository.class);
 
-        Long clubeMandanteId = 2L;
-        Long clubeVisitanteId = 3L;
-        Long estadioId = 1L;
-        LocalDateTime dataHora = LocalDateTime.now().plusDays(1);
+        when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", true, dataHora.minusYears(1))));
+        when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "visitante", false, dataHora.minusYears(1))));
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
 
-        Mockito.when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "mandante", true, dataHora.minusYears(1))));
-        Mockito.when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "visitante", false, dataHora.minusYears(1))));
-        Mockito.when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
-
-        PartidaService service = new PartidaService(repository, clubeRepository, estadioRepository);
         PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
 
-        assertThrows(ConflitoDadosException.class, () -> service.cadastrarPartida(request));
+        assertThrows(ConflitoDadosException.class, () -> partidaService.cadastrarPartida(request));
 
     }
 
     @Test
     void deveLancarExcecaoQuandoDataPartidaEhAnteriorDataCriacaoDeAlgumClube() {
-        PartidaRepository repository = Mockito.mock(PartidaRepository.class);
-        ClubeRepository clubeRepository = Mockito.mock(ClubeRepository.class);
-        EstadioRepository estadioRepository = Mockito.mock(EstadioRepository.class);
 
-        Long clubeMandanteId = 2L;
-        Long clubeVisitanteId = 3L;
-        Long estadioId = 1L;
-
-        LocalDateTime dataHora = LocalDateTime.now().plusDays(1);
         LocalDateTime dataCriacaoMandante = dataHora.plusDays(1);
         LocalDateTime dataCriacaoVisitante = dataHora.minusDays(1);
 
-        Mockito.when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "Mandante", true, dataCriacaoMandante)));
-        Mockito.when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "Visitante", true, dataCriacaoVisitante)));
-        Mockito.when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
+        when(clubeRepository.findById(clubeMandanteId)).thenReturn(Optional.of(new Clube(clubeMandanteId, "Mandante", true, dataCriacaoMandante)));
+        when(clubeRepository.findById(clubeVisitanteId)).thenReturn(Optional.of(new Clube(clubeVisitanteId, "Visitante", true, dataCriacaoVisitante)));
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
 
-        PartidaService service = new PartidaService(repository, clubeRepository, estadioRepository);
         PartidaRequestDto requet = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
 
-        assertThrows(ConflitoDadosException.class, ()-> service.cadastrarPartida(requet));
+        assertThrows(ConflitoDadosException.class, () -> partidaService.cadastrarPartida(requet));
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoPlacarMandanteEhNegativo() {
+
+        when((clubeRepository.findById(clubeMandanteId))).thenReturn(Optional.empty());
+        when((clubeRepository.findById(clubeVisitanteId))).thenReturn(Optional.empty());
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.empty());
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", true)));
+        PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
+        when(partidaRepository.existsByEstadioIdAndDataHora(estadioId, dataHora)).thenReturn(false);
+
+        assertThrows(RecursoNaoEncontradoException.class, () -> partidaService.cadastrarPartida(request));
+
+
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoPlacarVisitanteEhNegativo() {
+        PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, -1, dataHora);
+
+        assertThrows(PlacarInvalidoException.class, () -> partidaService.cadastrarPartida(request));
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoEstadioEstaInativo() {
+
+        when(estadioRepository.findById(estadioId)).thenReturn(Optional.of(new Estadio(estadioId, "Estádio", false)));
+
+        PartidaRequestDto request = new PartidaRequestDto(clubeMandanteId, clubeVisitanteId, estadioId, 0, 0, dataHora);
+
+        assertThrows(ConflitoDadosException.class, () -> partidaService.cadastrarPartida(request));
+
     }
 
 
